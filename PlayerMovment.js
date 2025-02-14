41369bf2be470efa6d4070fe532b820fe6f17a70
yosseferan95 <gokosecret@gmail.com>
2025-02-14 11:11:36 +0200

    function move_player

---
+++ PlayerMovment.js
@@ -0,0 +1,16 @@
+
+function MovePlayer (direction)
+{}
+  let Player = {
+    y : -4,
+    x : -6
+  }
+  new Promise((resolve, reject) => {
+    console.log('player is stadying')
+    resolve(direction)
+
+  })
+  .then((direction) => {
+    if (direction = )
+  })
+}
