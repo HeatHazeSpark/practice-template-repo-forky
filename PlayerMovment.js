d6e94a23467975cd902cefcda33a4e2947530bcb
yosseferan95 <gokosecret@gmail.com>
2025-02-14 15:10:31 +0200

    the final(0.0.2)

--- PlayerMovment.js
+++ PlayerMovment.js
@@ -8,25 +8,32 @@ function MovePlayer (direction,MoveBlocks){
     x : -6
   }
   new Promise((resolve, reject) => {
-    console.log('player is stadying')
-    resolve(direction,MoveBlocks)
-
+    console.log('player is steadying')
+    resolve({direction,MoveBlocks})
+    
   })
-  .then((direction,MoveBlocks) => {
-    if (direction = up){
-      Player.y + MoveBlocks
-    }else if (direction = down) {
-      Player.y - MoveBlocks
-    }else if (direction = right) {
-      Player.x + MoveBlocks
-    }else if (direction = left) {
-      Player.x - MoveBlocks
+  .then(({ direction, MoveBlocks}) => {
+    if (direction === up){
+      Player.y += MoveBlocks
+    }else if (direction === down) {
+      Player.y -= MoveBlocks
+    }else if (direction === right) {
+      Player.x += MoveBlocks
+    }else if (direction === left) {
+      Player.x -= MoveBlocks
     }else{
       console.log('please reasign the direction right or the Move Blocks')
     } 
       
+    return Player;
     
   })
-.then((Player)){console.log(`ur player is at ${Player.y},${Player.x}`)}
-.catch{console.log('error 404 , restart game please')}
+.then((Player) => {
+  console.log(`ur player is at ${Player.y},${Player.x}`)
+})
+.catch((error) => {
+  console.log('error 404 , restart game please',error)
+})
 }
+MovePlayer("up", 3);
+MovePlayer("right", 2);
