535f5a30aa108fad532bed176d723c238b221d42
yosseferan95 <gokosecret@gmail.com>
2025-02-14 11:32:18 +0200

    new update

--- PlayerMovment.js
+++ PlayerMovment.js
@@ -1,16 +1,31 @@
-
-function MovePlayer (direction)
-{}
+let up = "up"
+let down = "down"
+let right = "right"
+let left = "left"
+function MovePlayer (direction,MoveBlocks){
   let Player = {
     y : -4,
     x : -6
   }
   new Promise((resolve, reject) => {
     console.log('player is stadying')
-    resolve(direction)
+    resolve(direction,MoveBlocks)
 
   })
-  .then((direction) => {
-    if (direction = )
+  .then((direction,MoveBlocks) => {
+    if (direction = up){
+      Player.y + MoveBlocks
+    }if (direction = down) {
+      Player.y - MoveBlocks
+    }if (direction = right) {
+      Player.x + MoveBlocks
+    } if (direction = left) {
+      Player.x - MoveBlocks
+    }else{
+      console.log('please reasign the direction right or the Move Blocks')
+    } 
+      
+    
   })
+
 }
